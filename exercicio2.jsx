8dea3091ca0c4bebc1aacef48d287bcfe45560ca
Carlos Eduardo <138811560+Carlosalves18@users.noreply.github.com>
2024-03-27 18:21:34 -0300

    Add files via upload

---
+++ exercicio2.jsx
@@ -0,0 +1,45 @@
+const produtos = [
+    {
+      id: 1,
+      nome: 'Smartphone',
+      preco: 'R$ 2000',
+      cores: ['#29d8d5', '#252a34', '#fc3766'],
+    },
+    {
+      id: 2,
+      nome: 'Notebook',
+      preco: 'R$ 3000',
+      cores: ['#ffd045', '#d4394b', '#f37c59'],
+    },
+    {
+      id: 3,
+      nome: 'Tablet',
+      preco: 'R$ 1500',
+      cores: ['#365069', '#47c1c8', '#f95786'],
+    },
+  ];
+
+  const Exercicio2 = () =>{
+
+    const dadosP = produtos.filter((item) => (Number(item.preco.replace("R$","")) > 1500 ))
+
+    console.log(dadosP)
+
+    return(
+        <section>
+            {dadosP.map((item) =>
+            <>
+
+            <h1 key={item.id}>{item.nome}</h1>
+            <p key={item.id}>{item.preco}</p>
+            <p key={item.id}>{item.cores}</p>
+
+
+            </>
+            )}
+        </section>
+    )
+
+  };
+
+  export default Exercicio2;
---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Exercicio2 from './exercicio2'
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <Exercicio2/>
+  </React.StrictMode>,
+)
